6dab62bdc1ac48fbd5ccc324724605ba4f8cff5a
Arushi <arushijain866@gmail.com>
2026-08-21 15:18:03 +0530

    Time: 2 ms (49.82%) | Memory: 9.8 MB (26.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
+        int a = s.size();
+        int b = t.size();
+        if (a != b)
+            return false;
+        unordered_map<char, int> freq;
 
-        unordered_map<char,int> freq;
-        for(char el : s){
+        for (char el : s) {
             freq[el]++;
         }
-        for(char el : t){
+        for (char el : t) {
             freq[el]--;
-            if (freq[el]<0) return false;
+
+            if (freq[el] < 0) {
+                return false;
+            }
         }
         return true;
-    }
-};
+        }
+    };
